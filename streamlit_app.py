2e18b2876f62b530a158626085dcccdbdb3357e0
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-05-09 14:46:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,14 @@ uploaded_file = st.file_uploader("Drag and drop file here", type=['csv', 'xlsx',
 if st.button("Process"):
     if uploaded_file is not None:
         # Proses file di sini, misalnya membaca data
+        if uploaded_file.name.endswith('.csv'):
+            data = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith('.xlsx'):
+            data = pd.read_excel(uploaded_file)
+        elif uploaded_file.name.endswith('.txt'):
+            data = pd.read_csv(uploaded_file, sep="\t")  # Assuming tab-separated for txt files
         st.success("File berhasil diproses!")
-        # Tampilkan informasi atau hasil analisis dari file jika diperlukan
+        # Tampilkan tabel hasil
+        st.dataframe(data)
     else:
         st.warning("Silakan unggah file terlebih dahulu.")
