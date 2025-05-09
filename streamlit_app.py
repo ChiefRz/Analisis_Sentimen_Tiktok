dbc821312d2013f6845a32b2a31121d111ed4810
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-05-09 14:41:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Pengaturan sidebar
+st.sidebar.title("Menu")
+st.sidebar.write("Upload Data")
+st.sidebar.write("Preprocessing Data")
+st.sidebar.write("Hasil Analisis")
+
+# Judul utama
+st.title("Upload your file here:")
+
+# Upload file
+uploaded_file = st.file_uploader("Drag and drop file here", type=['csv', 'xlsx', 'txt'], label_visibility="collapsed")
+
+# Tombol Process
+if st.button("Process"):
+    if uploaded_file is not None:
+        # Proses file di sini, misalnya membaca data
+        st.success("File berhasil diproses!")
+        # Tampilkan informasi atau hasil analisis dari file jika diperlukan
+    else:
+        st.warning("Silakan unggah file terlebih dahulu.")
