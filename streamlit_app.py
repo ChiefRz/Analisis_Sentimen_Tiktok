8aa523ef26fab96d6f305e1fba627f6b2a381da0
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-05-10 11:31:22 +0700

    UPDATE

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,14 @@ choice = st.sidebar.radio("Select an option", menu_options)
 # Main content based on selected option
 if choice == "Upload Data":
     uploaded_file = st.file_uploader("Upload your files here:", type=['csv', 'xlsx'])
-    st.button("Process"): 
-    if uploaded_file is not None: 
+    st.button("Process"): if uploaded_file is not None: 
     # Proses file di sini, misalnya membaca data 
-        if uploaded_file.name.endswith('.csv'): 
-            data = pd.read_csv(uploaded_file) 
-        elif uploaded_file.name.endswith('.xlsx'): 
-             data = pd.read_excel(uploaded_file) 
-        elif uploaded_file.name.endswith('.txt'): 
-             data = pd.read_csv(uploaded_file, sep="\t") 
+    if uploaded_file.name.endswith('.csv'): 
+        data = pd.read_csv(uploaded_file) 
+    elif uploaded_file.name.endswith('.xlsx'): 
+        data = pd.read_excel(uploaded_file) 
+    elif uploaded_file.name.endswith('.txt'): 
+        data = pd.read_csv(uploaded_file, sep="\t") 
     
     # Assuming tab-separated for txt files 
     st.success("File berhasil diproses!") 
