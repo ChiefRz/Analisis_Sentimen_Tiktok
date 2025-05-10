bcbf8f6d22c9b03f1f2819faed492bf226d5b24b
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-05-10 11:22:35 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,7 @@ st.title("Pick your file here:")
 
 # Main content based on selected option
 if choice == "Upload Data":
-    st.write("Pick your file here:")
-    uploaded_file = st.file_uploader("Select your files here:", type=['csv', 'xlsx'])
+    uploaded_file = st.file_uploader("Upload your files here:", type=['csv', 'xlsx'])
     if uploaded_file is not None:
         # You can add code to read and process the uploaded file here
         st.success("File uploaded successfully!")
